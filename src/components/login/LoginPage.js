4749c5e08edbb82bdb1e60eba420ae80f97c9d9d
Erik Lange <erik.lange@fasttvnet.dk>
2023-03-24 17:24:59 +0100

    user can go to login page when signed ud

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -3,16 +3,12 @@ import { useState } from "react";
 export default function LoginPage(props) {
     const [textInput, setTextInput] = useState('');
 
-    function handleTextareaChange(e) {
-        setTextInput(e.target.value);
-    }
-
     return (
         <>
             <h1>Welcome to tiktak!</h1>
             <p>Log in to start playing.</p>
             <form onSubmit={() => props.onLoginAttempt(textInput)}>
-                <textarea value={textInput} onChange={handleTextareaChange}/>
+                <input value={textInput} onChange={e => setTextInput(e.target.value)}/>
                 <button>Login</button>
             </form>
             <p>Not on tiktak yet?<button onClick={props.onClickSignup}>Sign up</button></p>
--- src/components/login/SignupPage.js
+++ src/components/login/SignupPage.js
@@ -2,21 +2,35 @@ import { useState } from "react";
 
 export default function SignupPage(props) {
     const [username, setUsername] = useState('');
+    const [status, setStatus] = useState('signingUp'); // possible values are 'signingUp', 'success', 'failure'
 
     function createUser(e, name) {
         e.preventDefault();
         // ask the server to create user with username='name'
-        console.log('Creating user...')
+        console.log('Creating user...');
+        console.log(`Successfully created user ${name}`);
+        setStatus('success');
     }
 
-    return (
-        <>
-            <p>Choose a username</p>
-            <form onSubmit={e => createUser(e, username)}>
-                <textarea value={username} onChange={e => setUsername(e.target.value)}/>
-                <button>Sign up</button>
-            </form>
-            <p>Already on tiktak?<button onClick={props.onClickLogin}>Log in</button></p>
-        </>
-    );
+    if (status === 'success') {
+        return (
+            <>
+                <p>You are now signed up, '{username}'. Welcome on board!</p>
+                <button onClick={props.onClickLogin}>Go to login page</button>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <p>Choose a username</p>
+                <form onSubmit={e => createUser(e, username)}>
+                    <input value={username} onChange={e => setUsername(e.target.value)}/>
+                    <button>Sign up</button>
+                    { status === 'failure' && <p>Sign-up failed. Try again.</p> }
+                </form>
+                <p>Already on tiktak?<button onClick={props.onClickLogin}>Log in</button></p>
+            </>
+        );
+    }
+
 }
