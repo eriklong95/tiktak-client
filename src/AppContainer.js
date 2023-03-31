1a7519d5f6ce1c1e3d1e0c3b7c0b74550855a24b
Erik Lange <erik.lange@fasttvnet.dk>
2023-03-31 07:52:24 +0200

    stubbing dialog opens on server calls in stub mode

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -4,33 +4,37 @@ import ServerConnectionConfig from "./ServerConnectionConfig";
 import StubbingDialog from "./StubbingDialog";
 
 export default function AppContainer() {
+    const [stubbingDialogOpen, setStubbingDialogOpen] = useState(false);
+    function stubFetch(request) {
+        setStubbingDialogOpen(true);
+        return new Promise((resolve, reject) => {
+            reject('Network error');
+        });
+    }
+
+    const initialServerConnection = {
+        host: 'http://localhost:5000',
+        stubResponses: false,
+    
+        // expects Fetch API Request, returns Promise of Fetch API response
+        call: function (request) {
+            if (this.stubResponses) {
+                return stubFetch(request)
+            } else {
+                return fetch(request);
+            }
+        },
+    
+    };
+
     const [serverConnection, setServerConnection] = useState(initialServerConnection);
 
     return (
         <>
             <App serverConnection={serverConnection}/>
             <ServerConnectionConfig setServerConnection={setServerConnection} />
-            <StubbingDialog />
+            <StubbingDialog open={stubbingDialogOpen} setOpen={setStubbingDialogOpen} />
         </>
     )
 }
 
-const initialServerConnection = {
-    host: 'http://localhost:5000',
-    stubResponses: false,
-
-    // expects Fetch API Request, returns Promise of Fetch API response
-    call: function(request) {
-        if (this.stubResponses) {
-            return stubFetch(request)
-        } else {
-            return fetch(request);
-        }
-    }
-}
-
-function stubFetch(request) {
-    return new Promise((resolve, reject) => {
-        reject('Network error');
-    })
-}
--- src/StubbingDialog.js
+++ src/StubbingDialog.js
@@ -1,5 +1,8 @@
-export default function StubbingDialog() {
+export default function StubbingDialog(props) {
     return (
-        <dialog open={false}>Configure stubbing</dialog>
+        <dialog open={props.open}>
+            <p>Configure stubbing</p>
+            <button onClick={() => props.setOpen(false)}>OK</button>
+        </dialog>
     )
 }
