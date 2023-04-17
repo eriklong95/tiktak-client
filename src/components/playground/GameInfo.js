c53b239897e717129ca317ec8314039c051470e9
Erik Lange <erik.lange@fasttvnet.dk>
2023-04-17 19:23:02 +0200

    styles playground

--- src/components/playground/GameInfo.js
+++ src/components/playground/GameInfo.js
@@ -1,19 +1,22 @@
+import styles from "./gameinfo.module.css";
+
 function GameInfo(props) {
     if (props.game === null) {
         return (
-            <section>
+            <section className={styles.gameinfo}>
                 <p>Failed to load game details</p>
             </section>
         )
     }
 
     const userIsA = props.game.playerA === props.user;
+    const opponentUsername = userIsA ? props.game.playerB : props.game.playerB;
     const myTurn = (userIsA && props.game.gameState.turn === 'A') || (!userIsA && props.game.gameState.turn === 'B');
 
     return (
-        <section>
+        <section className={styles.gameinfo}>
             <p>Welcome to the game, {props.user}!</p>
-            <p>You are {userIsA ? 'A' : 'B'} in this game. Your opponent is {userIsA ? props.game.playerB : props.game.playerB}.</p>
+            <p>You are {userIsA ? 'A' : 'B'} in this game. Your opponent is {opponentUsername}.</p>
             <p>It is {myTurn ? '' : 'not'} your turn.</p>
         </section>
     );
